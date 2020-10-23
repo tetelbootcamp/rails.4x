068d41c9fe92d20a3a6be5b63bf4230e0174c556
tetel.player@gmail.com <tetel.player@gmail.com>
2020-10-23 15:44:17 -0300

    create a layouts.

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,7 @@
 class Backoffice::CategoriesController < ApplicationController
+  before_action :authenticate_admin!
+  layout "backoffice"  
   def index
+    @categories = Category.all
   end
 end
