8e743d0e8282c4e0ead781f224bc4605cb1c187c
tetel.player@gmail.com <tetel.player@gmail.com>
2020-10-26 14:59:04 -0300

    criado o new e o create de categorias.

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -4,9 +4,26 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
+    @category = Category.new
   end
+
+  def create
+    @category = Category.new(params_category)
+    if @category.save
+      redirect_to backoffice_categories_path, notice: "a Categoria (#{@category.description}) foi salva"
+    else
+      render :new
+    end
+  end
+  
 
   def update
   end
-  
+
+  private
+
+  def params_category
+    params.require(:category).permit(:description)
+  end
+
 end
