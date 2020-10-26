f6c3deef9868d5d492c0fd7fdcd7c3b8263905d9
tetel.player@gmail.com <tetel.player@gmail.com>
2020-10-26 15:12:25 -0300

    criado o edit e update.

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,7 @@
-class Backoffice::CategoriesController < BackofficeController  
+class Backoffice::CategoriesController < BackofficeController
+
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
@@ -16,14 +19,26 @@ class Backoffice::CategoriesController < BackofficeController
     end
   end
   
+  def edit
+
+  end
 
   def update
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "a Categoria (#{@category.description}) foi atualizada com sucesso!"
+    else
+      render :edit
+    end
   end
 
   private
 
-  def params_category
-    params.require(:category).permit(:description)
-  end
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def params_category
+      params.require(:category).permit(:description)
+    end
 
 end
